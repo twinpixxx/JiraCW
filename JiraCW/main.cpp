8698b6d031b9d884833626c289c6c541314446ca
Anthony Borushko <twinpixxx2005@gmail.com>
2019-12-16 22:50:16 +0300

    feat: console window color, position and width/height

--- JiraCW/main.cpp
+++ JiraCW/main.cpp
@@ -19,28 +19,37 @@
 
 using namespace std;
 
-int main()
-{
-	// add russian locale
+
+int main() {
+	//	Add russian locale
 	setlocale(LC_ALL, "Russian");
-	
-	//get console window and some size/console settings
-	HWND consoleWindow = GetConsoleWindow();
+	system("color F0"); //	Color of console window
+
+	//	Get console window and some size/console settings
+	HWND consoleWindow = GetForegroundWindow();
 	RECT ConsoleRect;
 	GetWindowRect(consoleWindow, &ConsoleRect);
-	MoveWindow(consoleWindow, ConsoleRect.left, ConsoleRect.top, 1400, 720, TRUE);
-	HWND preventClosing = GetForegroundWindow();	//prevent error console closing
-	EnableWindow(preventClosing, false);
+	MoveWindow(consoleWindow, ConsoleRect.left, ConsoleRect.top, 1700, 720, TRUE);
 
 	//	Getting the desktop hadle and rectangule
 	HWND   hwndScreen;
+	RECT   rectScreen;
+	hwndScreen = GetDesktopWindow();
+	GetWindowRect(hwndScreen, &rectScreen);
+	EnableWindow(consoleWindow, false);
 
 	//	Get the current width and height of the console
 	RECT rConsole;
+	GetWindowRect(consoleWindow, &rConsole);
+	int Width = rConsole.left = rConsole.right;
+	int Height = rConsole.bottom - rConsole.top;
 
 	//	Caculate the window console to center of the screen	
 	int ConsolePosX;
 	int ConsolePosY;
+	ConsolePosX = ((rectScreen.right - rectScreen.left) / 2 - Width / 2);
+	ConsolePosY = ((rectScreen.bottom - rectScreen.top) / 2 - Height / 2);
+	SetWindowPos(consoleWindow, NULL, ConsolePosX, ConsolePosY, Width, Height, SWP_SHOWWINDOW || SWP_NOSIZE);
 
 
 
